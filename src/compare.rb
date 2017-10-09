80c06d1b1f5ad746a99ed77851081b6fbe39037f
Orta Therox <orta.therox@gmail.com>
2017-10-08 21:28:43 -0400

    Fix more syntax

--- src/compare.rb
+++ src/compare.rb
@@ -9,8 +9,11 @@ result = GraphQL::SchemaComparator.compare(parsed_old, parsed_new)
 notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"]
 
 if result.identical?
-  notifier.post "No changes to Metaphysics"
+  notifier.post text: "No changes to Metaphysics"
 else
-  msgs = result.changes.map { |_c| change.breaking ? "⚠️  #{change.message}" : "✅  #{change.message}" }
-  notifier.post "Metaphysics: \n\n#{msgs.join("\n")}"
+  msgs = result
+         .changes
+         .map { |c| c.breaking ? "⚠️  #{c.message}" : "✅  #{c.message}" }
+
+  notifier.post text: "Metaphysics: \n\n#{msgs.join("\n")}"
 end
